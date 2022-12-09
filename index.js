ea8471eaf04fcbee16c063265a32e03d1e2d356d
Nikita Kravchenko <89710564+NicKravchenko@users.noreply.github.com>
2022-12-09 17:07:18 -0400

    Recursive screen function, defined screeen structure

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ var activeScreenName = "showWelcomeScreen";
 
 var rawdata = fs.readFileSync("config.json");
 var config = JSON.parse(rawdata);
+let repeatLogic = true;
+let activeScreen = [];
 
 const screenBuilder = (activeScreenName) => {
   const conf = config;
-  let activeScreen = [];
 
   conf.forEach((element) => {
     if (activeScreenName === element.screenName) {
@@ -18,7 +19,31 @@ const screenBuilder = (activeScreenName) => {
     }
   });
 
-  console.log(activeScreen);
+  const { content } = activeScreen;
+  const { screenMessage } = content;
+  const { actions } = content;
+
+  // console.log(activeScreen);
+  console.log();
+
+  console.log(screenMessage);
+
+  console.log("Go to next screen:");
+
+  actions.forEach((element) => {
+    console.log(element);
+  });
+
+  const input = prompt("Screen: ");
+
+  if (input == "q") return;
+
+  actions.forEach((element) => {
+    if (element.button == input) {
+      activeScreenName = element.screenName;
+      screenBuilder(activeScreenName);
+    }
+  });
 };
 
 screenBuilder(activeScreenName);
