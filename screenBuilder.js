d0ea5468d820416f144940e1bdbc6558e4dceba5
Nikita Kravchenko <89710564+NicKravchenko@users.noreply.github.com>
2022-12-10 01:33:21 -0400

    Nothing works

--- screenBuilder.js
+++ screenBuilder.js
@@ -1,23 +1,40 @@
 const fs = require("fs");
 const prompt = require("prompt-sync")();
+
 var rawdata = fs.readFileSync(`${__dirname}/config.json`);
 var config = JSON.parse(rawdata);
 
-function buildScreenByType(screenToBuild) {}
-
-function buildInfoScreen(activeScreenName) {
+function getScreenToShow(screenToShowName) {
   const conf = config;
-  let activeScreen = [];
+  let screenToShow = [];
 
-  //Choose active screen
+  //Choose  screen to show
   conf.forEach((element) => {
-    if (activeScreenName === element.screenName) {
-      activeScreen = element;
+    if (screenToShowName === element.screenName) {
+      screenToShow = element;
     }
   });
 
+  return screenToShow;
+}
+
+function showScreenByName(screenToShowName) {
+  const screenToShow = getScreenToShow(screenToShowName);
+
+  const { type } = screenToShow;
+  console.log("Im in show screen by name");
+  switch (type) {
+    case "info":
+      showInfoScreen(screenToShow);
+    case "get":
+      showGetScreen(screenToShow);
+  }
+}
+
+function showInfoScreen(screenToShow) {
   //Define screen parts
-  const { content } = activeScreen;
+  let nextScreenToShowName = "";
+  const { content } = screenToShow;
   const { screenMessage } = content;
   const { actions } = content;
 
@@ -33,17 +50,60 @@ function buildInfoScreen(activeScreenName) {
   const input = prompt("Screen: ");
 
   //Exit if pressed q
-  if (input == "q") return;
-
-  //Call screen
-  actions.forEach((element) => {
-    if (element.button == input) {
-      activeScreenName = element.screenName;
-      buildInfoScreen(activeScreenName);
-    }
-  });
+  if (input == "q") {
+    console.log("q pressed");
+  } else {
+    //Call screen
+    actions.forEach((element) => {
+      if (element.button === input) {
+        nextScreenToShowName = element.screenName;
+        showScreenByName(nextScreenToShowName);
+      } else {
+        showScreenByName("show404Screen");
+      }
+    });
+  }
 }
 
+function showGetScreen(screenToShow) {
+  //Define screen parts
+  let nextScreenToShowName = "";
+  const { content } = screenToShow;
+  const { screenMessage } = content;
+  const { actions } = content;
+
+  console.log(screenMessage);
+
+  console.log("Go to next screen:");
+
+  //Show possible actions
+  actions.forEach((element) => {
+    console.log(element);
+  });
+
+  const input = prompt("Screen: ");
+
+  //Exit if pressed q
+  if (input == "q") {
+    console.log("q pressed");
+    return;
+  } else {
+    //Call screen
+    actions.forEach((element) => {
+      if (element.button === input) {
+        nextScreenToShowName = element.screenName;
+        showScreenByName(nextScreenToShowName);
+      } else {
+        showScreenByName("show404Screen");
+      }
+      return;
+    });
+  }
+}
+
+showScreenByName("showWelcomeScreen");
+
 module.exports = function screenBuilder(activeScreenName) {
-  buildInfoScreen(activeScreenName);
+  //   showInfoScreen(activeScreenName);
+  showScreenByName(activeScreenName);
 };
